9618d2d78b0643db8f5e17c32bf245c7f4b2a797
Wunmin Wong <wunmin@gmail.com>
2015-08-27 11:21:23 +0800

    add model validation and callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posttags
   has_many :tags, through: :posttags
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,15 @@ class Tag < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posttags
   has_many :posts, through: :posttags
+
+  validates :tag, presence: true
+
+  before_save :downcase_tag
+
+
+  private
+
+  def downcase_tag
+    self.tag = self.tag.downcase
+  end
 end
